1725bdc9ef2e4fbe921b2877596bc1476c2a32c4
ThabisoMagwaza <836403@students.wits.ac.za>
2019-03-11 12:38:22 +0200

    about page added

--- index.js
+++ index.js
@@ -1,9 +1,14 @@
-// let path = require('path')
+let path = require('path')
 let express = require('express')
 let app = express()
-let mainRouter = require('/mainRoutes.js')
 
-app.use(mainRouter)
+app.get('/', function (req, res) {
+  res.send('Hello World')
+})
+
+app.get('/about', function (req, res) {
+  res.sendFile(path.join(__dirname, 'views', 'about.html'))
+})
 
 app.listen(3000)
-console.log('Express server running on port 3000')
+console.log('Express server is running on port 3000')
